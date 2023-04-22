cf3de02b68edf2f27f90307ef257f58b30ddaacf
mehmetozdeemiir <75038861+mehmetozdeemiir@users.noreply.github.com>
2023-04-22 18:47:10 +0300

    created open api

--- src/main/java/com/project/weatherapi/controller/WeatherController.java
+++ src/main/java/com/project/weatherapi/controller/WeatherController.java
@@ -4,9 +4,16 @@ import com.project.weatherapi.dto.WeatherDto;
 import com.project.weatherapi.service.WeatherService;
 import com.project.weatherapi.validation.CityNameConstraint;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +25,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/api/weather")
 @AllArgsConstructor
 @Validated//string değilde obje olsaydı valid calısırdı ama string oldugu için validated koymak zorunda kaldık
+@Tag(name = "Open Weather Service API v1", description = "Open Weather Service API to search the current weather report of the city")
 public class WeatherController {
     private final WeatherService weatherService;
 
+
+    @Operation(
+            method = "GET",
+            summary = "search the current weather report of the city",
+            description = "search the current weather report of the city name filter. The api has the rate limiting. 10 request per minute",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "The current weather report of the city",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = WeatherDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "City name is wrong. Re-try with a valid city name",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+                    @ApiResponse(
+                            responseCode = "429",
+                            description = "Rate limit exceeded. Please try your request again later!",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal/External server error.",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @GetMapping("/{city}")
     @RateLimiter(name = "basic")
     public ResponseEntity<WeatherDto> getWeather(@PathVariable("city") @Valid @CityNameConstraint @NotBlank String city){
